c05861b1d88acf0abebb009935b728e641cc1a2d
fanleyyy <158832161+fanleyyy@users.noreply.github.com>
2024-08-07 10:33:34 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector('.login-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    // TO DO: implement login logic here
+    console.log('Login form submitted!');
+});
